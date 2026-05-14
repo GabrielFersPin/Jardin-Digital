65457953cc1855c7bb7c3222be9ec57a9cdff097
GabrielFerSpin <gabrielfelipef.23@gmail.com>
2026-05-14 15:58:31 +0200

    Quartz sync: May 14, 2026, 3:58 PM

--- sincronizar.sh
+++ sincronizar.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 1. Cargar el entorno de Node/NVM para que Obsidian use la versión correcta
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Intentar usar la versión v22 que es la que tienes en tu terminal
+nvm use v22 --silent || nvm use default --silent
+
 echo "🔄 Copiando notas desde tu Segundo Cerebro..."
 # Sincronizamos las carpetas de forma exacta, borrando lo que ya no esté y excluyendo .git
 rsync -a --delete --exclude='.git' --exclude='.obsidian' /home/gabriel/Documents/Segundo_Cerebro/ /home/gabriel/quartz/content/
